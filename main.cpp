0076864d91f84416a8086ca029dc27f77868c395
Urdhva <urdhva.borse@gmail.com>
2024-10-05 11:42:32 +0530

    completed hardware phase of the project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,53 @@
+//funny how we can simply copy past C's code and it still is valid
+
+//TO CONTINUE - Condition Flags
+
 #include <iostream>
+#define MEMORY_MAX (1 << 16)
+
+//basically __UINT16_TYPE__
+short unsigned int memory[MEMORY_MAX];
+
+enum Registors
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+enum Opcodes
+{
+    OP_BR = 0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+};
+
+short unsigned int reg[R_COUNT];
 
 int main()
 {
-    std::cout << '\n';
+    std::cout << "This is the way\n";
 
     return 0;
 }
--- test.c
+++ test.c
@@ -1,5 +1,37 @@
-// #include <stdio.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <Windows.h>
+#include <conio.h>
 #define MEMORY_MAX (1 << 16)
+
+
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+void disable_input_buffering()
+{
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode); /* save old mode */
+    fdwMode = fdwOldMode
+            ^ ENABLE_ECHO_INPUT  /* no input echo */
+            ^ ENABLE_LINE_INPUT; /* return when one or
+                                    more characters are available */
+    SetConsoleMode(hStdin, fdwMode); /* set new mode */
+    FlushConsoleInputBuffer(hStdin); /* clear buffer */
+}
+
+void restore_input_buffering()
+{
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+uint16_t check_key()
+{
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
+
+
 //unsigned integer of 16 bits
 __UINT16_TYPE__ memory[MEMORY_MAX];
 
@@ -38,13 +70,17 @@ enum Opcodes
     OP_TRAP         //execute trap
 };
 
+enum Condition_Flags
+{
+    FL_POS = 1 << 0,    //P
+    FL_ZRO = 1 << 1,    //Z
+    FL_NEG = 1 << 2,    //N
+};
+
 //registors will be stored in an array:
 __UINT16_TYPE__ reg[R_COUNT];
 
-int main(){
-    printf("Helo");
-
-
-
-    return 0;
+int main()
+{   
+        
 }
